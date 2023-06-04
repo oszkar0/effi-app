69c0f1480310fd6876e4b4656fd25d31fd87fcf4
Oskar <oskszysiak@gmail.com>
2023-06-04 22:07:49 +0200

    initial commit

---
+++ src/main/java/com/effi/EffiApp/EffiAppApplication.java
@@ -0,0 +1,13 @@
+package com.effi.EffiApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EffiAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/effi/EffiApp/EffiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.effi.EffiApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
