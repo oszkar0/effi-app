8db1f7ea4187b2aad0f5e3eb48e49496801a8cc5
Oskar <oskszysiak@gmail.com>
2023-06-07 15:05:43 +0200

    added security config class, which provides beans for: bcrypt encoder, dao authentication provider and security filter chain

---
+++ src/main/java/com/effi/EffiApp/security/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.effi.EffiApp.security;
+
+import com.effi.EffiApp.service.UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    //bcrypt bean
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    //authenticantion provider bean
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(UserService userService){
+        DaoAuthenticationProvider daoAuthProvider = new DaoAuthenticationProvider();
+        daoAuthProvider.setPasswordEncoder(bCryptPasswordEncoder());
+        daoAuthProvider.setUserDetailsService(userService);
+        return daoAuthProvider;
+    }
+
+    //configure security filter chain
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity
+                .authorizeHttpRequests(configurer -> configurer
+                        .requestMatchers("/").hasRole("EMPLOYEE") //config endpoints permisions
+                        .anyRequest().authenticated())
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .usernameParameter("email")
+                        .loginProcessingUrl("/process-login")
+                        .defaultSuccessUrl("/success")
+                        .permitAll()) //set login page etc.
+                .logout(logout -> logout
+                        .permitAll()) //permit all for logging out
+                .exceptionHandling(configurer -> configurer
+                        .accessDeniedPage("/access-denies")); //set access denied page
+
+        return httpSecurity.build();
+    }
+
+}
