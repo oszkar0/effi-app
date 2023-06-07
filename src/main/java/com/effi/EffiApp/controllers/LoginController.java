ea99ea9ab74f4c2408d3c95c5b8c49f93143d14d
Oskar <oskszysiak@gmail.com>
2023-06-07 15:11:07 +0200

    create controller class with get mappings for login and success login page

---
+++ src/main/java/com/effi/EffiApp/controllers/LoginController.java
@@ -0,0 +1,17 @@
+package com.effi.EffiApp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public String getLoginPage(){
+        return "login";
+    }
+
+    @GetMapping("/success")
+    public String getSuccessPage(){
+        return "success-page";
+    }
+}
