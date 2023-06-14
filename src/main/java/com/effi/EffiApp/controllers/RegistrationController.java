616c6993805fbad880d55558583347370074a0f7
Oskar <oskszysiak@gmail.com>
2023-06-14 18:14:59 +0200

    add method to process registartion form

--- src/main/java/com/effi/EffiApp/controllers/RegistrationController.java
+++ src/main/java/com/effi/EffiApp/controllers/RegistrationController.java
@@ -1,18 +1,50 @@
 package com.effi.EffiApp.controllers;
 
 import com.effi.EffiApp.registration.RegistartionObject;
+import com.effi.EffiApp.service.UserService;
+import org.apache.juli.logging.Log;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.logging.Logger;
 
 @Controller
 @RequestMapping("/register")
 public class RegistrationController {
+    //create logger for information purposes
+    private Logger logger = Logger.getLogger(getClass().getName());
+    private UserService userService;
+
+    @Autowired
+    public RegistrationController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/show-registration-form")
     public String getRegistrationForm(Model model){
         model.addAttribute("registrationObject", new RegistartionObject());
 
         return "registration-form";
+    }
+
+    @PostMapping("/process-registration")
+    public String processRegistration(@ModelAttribute("registrationObject") RegistartionObject registrationObject){
+        logger.info("Processing user: " + registrationObject.getUserEmail());
+
+        if(userService.userExists(registrationObject.getUserEmail())){
+            logger.warning("User already exists");
+            return "redirect:/login";
+        }
+
+        userService.save(registrationObject);
+
+        logger.info("Successfully saved user " + registrationObject.getUserEmail()  + " to db");
+
+        return "redirect:/login";
     }
 }
