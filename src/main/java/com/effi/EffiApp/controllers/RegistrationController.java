d7eac1381b0ea5bc290ba0c7975f071840751ebf
Oskar <oskszysiak@gmail.com>
2023-06-21 19:52:18 +0200

    create employee registration processing method

--- src/main/java/com/effi/EffiApp/controllers/RegistrationController.java
+++ src/main/java/com/effi/EffiApp/controllers/RegistrationController.java
@@ -1,10 +1,14 @@
 package com.effi.EffiApp.controllers;
 
+import com.effi.EffiApp.passwords.PasswordGenerator;
 import com.effi.EffiApp.registration.employee.EmployeeRegistrationObject;
 import com.effi.EffiApp.registration.owner.OwnerRegistrationObject;
+import com.effi.EffiApp.security.PrincipalInformation;
 import com.effi.EffiApp.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -63,5 +67,39 @@ public class RegistrationController {
 
         return "employee-form";
     }
-    
+
+    @PostMapping("/process-employee-registration")
+    public String processEmployeeRegistration(
+            @Valid @ModelAttribute("registrationObject") EmployeeRegistrationObject employeeRegistrationObject,
+            BindingResult bindingResult){
+
+        logger.info("Processing user: " + employeeRegistrationObject.getEmail());
+
+        //check if errors occurred, if occurred return filled form with error messages
+        if(bindingResult.hasErrors()){
+            return "employee-form";
+        }
+
+        //check if user is already in db
+        if(userService.userExists(employeeRegistrationObject.getEmail())){
+            logger.warning("User already exists");
+            return "redirect:/main-page";
+        }
+
+        //generate and set random password which can be changed by employee later
+        String password = PasswordGenerator.generatePassword(10);
+        employeeRegistrationObject.setPassword(password);
+
+        //get the logged user
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        PrincipalInformation loggedUser = (PrincipalInformation) authentication.getPrincipal();
+
+        //set new employee's company to admins(owners) company
+        employeeRegistrationObject.setCompany(loggedUser.getCompany());
+
+        userService.save(employeeRegistrationObject);
+
+        //todo: redirect the admin to new user info page
+        return "redirect:/main-page";
+    }
 }
