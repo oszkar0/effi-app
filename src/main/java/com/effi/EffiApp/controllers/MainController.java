d9ea29dbe093d76d47bfcedea1392e2d7900fd5b
oszkar0 <oskszysiak@gmail.com>
2023-06-25 01:37:17 +0200

    change the way principal is aquired, as the previous way could cause exceptions

--- src/main/java/com/effi/EffiApp/controllers/MainController.java
+++ src/main/java/com/effi/EffiApp/controllers/MainController.java
@@ -2,8 +2,11 @@ package com.effi.EffiApp.controllers;
 
 import com.effi.EffiApp.entity.User;
 import com.effi.EffiApp.security.PrincipalInformation;
+import com.effi.EffiApp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,14 +16,28 @@ import java.util.List;
 
 @Controller
 public class MainController {
+    private UserService userService;
+    @Autowired
+    public MainController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/main-page")
     public String getMainPage(Model model){
         //get the logged user
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        PrincipalInformation loggedUser = (PrincipalInformation) authentication.getPrincipal();
+        Object loggedUser = authentication.getPrincipal();
+        PrincipalInformation principalInformation = null;
+
+        if (loggedUser instanceof UserDetails) {
+            principalInformation = ((PrincipalInformation)loggedUser);
+        } else {
+            String email = loggedUser.toString();
+            principalInformation = (PrincipalInformation) userService.loadUserByUsername(email);
+        }
 
         //get all users from logged users company 
-        List<User> companyUsers = loggedUser.getCompany().getUsers();
+        List<User> companyUsers = principalInformation.getCompany().getUsers();
 
         model.addAttribute("users", companyUsers);
 
